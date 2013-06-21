6f3abc856a8c36ecbde7978759b7182b62376e77
shabindasmk <shabindasmk@gmail.com>
2013-06-21 14:41:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <div style="width: 100%;margin-left: 30%">
+            <div>
+                <img src="images/watch.jpg" width="150" height="200">
+                <div style="width:100px;margin-left: 50px;">$50.00</div>
+                <div style="width:100px;background-color: #CCCCCC;margin-left: 25px;margin-top:35px"><a href="checkout.php" style="text-align:center;text-decoration: none;color:black;font-weight:bold;padding:8px;">Buy Now</a></div>
+            </div>
+        </div>
+    </body>
+</html>
