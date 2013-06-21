62bad5f4b38d555f7568548cefc995c489d19295
shabindasmk <shabindasmk@gmail.com>
2013-06-21 14:41:00 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Define Constants
+*/
+define("PAYPAL_PRO_ENV","sandbox");//// 'sandbox' or 'beta-sandbox' or 'live'
+define("PAYPAL_PRO_API_UNAME","shabindasmk_api1.gmail.com");
+define("PAYPAL_PRO_API_PWD", "1371795904");
+define("PAYPAL_PRO_API_SIG", "AIG.4y.kCF4JbiX1PB8.pNVNzhFTAB8fQYi9HjWKmug6PRiyb5pQZ3f9");
+define("PAYPAL_PRO_API_URL", "https://api-3t.paypal.com/nvp");
+define("PAYPAL_PRO_API_VER", "76.0");
+define("PAYPAL_PAY_CURRENCY", "USD");
+define("PAYPAL_PAY_TYPE", "Sale");
+
+
+?>
